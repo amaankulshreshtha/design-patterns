395b5c56abe5ae19b5d62f5beb0e0515381e0e1b
Amaan Kulshreshtha <amaan.kulshreshtha@gmail.com>
2019-05-09 09:27:08 +0400

    add factory pattern

---
+++ creational-design-patterns/factory-pattern/README.md
@@ -0,0 +1,6 @@
+# Factory Pattern
+
+- More control over customizing an item
+- Extract the object creation and making the implementing object completely unaware of the creation process
+- We can extend the created object by adding changes in the implemented object and keeping the created object pristine
+- Makes it easy to update and add more versions or updates to the object 
---
+++ creational-design-patterns/factory-pattern/js/script.js
@@ -0,0 +1,61 @@
+(function (win, document, $) {
+	var CircleGenerator = (function () {
+		// The only public member outside the scope of this function would be this instance variable
+		var instance;
+
+		function init() {
+			var _aCircle = [],
+				_stage = $('.advert');
+
+			function _position(circle, top, left) {
+				circle.css('top', top);
+				circle.css('left', left);
+			}
+
+			function create(top, left) {
+				var circle = $('<div class="circle"></div>');
+				_position(circle, top, left);
+				return circle;
+			}
+
+			function add(circle) {
+				_stage.append(circle);
+				_aCircle.push(circle);
+			}
+
+			// This will tell us which index are we on in the array
+			function index() {
+				return _aCircle.length;
+			}
+
+			return {
+				create: create,
+				add: add,
+				index: index
+			};
+		}
+
+		return function () {
+			if (!instance) {
+				instance = init();
+			}
+			return instance;
+		};
+
+	})()
+	$(document).ready(function () {
+		$('.advert').click(function (e) {
+			var cg = CircleGenerator();
+			var circle = cg.create(e.pageY - 25, e.pageX - 25);
+			cg.add(circle);
+		});
+		$(document).keypress(function (e) {
+			if (e.key === 'a') {
+				var cg = CircleGenerator();
+				var circle = cg.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 500));
+				cg.add(circle);
+			};
+		});
+
+	});
+})(window, document, jQuery);
